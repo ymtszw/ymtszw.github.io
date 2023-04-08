7baf4b76332c3c8e3098b891e52d0446c346e362
Yu Matsuzawa <ymtszw@gmail.com>
2023-04-08 23:53:09 +0900

    feat: Indexページを移行

--- app/Route/Index.elm
+++ app/Route/Index.elm
@@ -1,22 +1,26 @@
-module Route.Index exposing (ActionData, Data, Model, Msg, RouteParams, route)
+module Route.Index exposing
+    ( ActionData
+    , Data
+    , Model
+    , Msg
+    , RouteParams
+    , route
+    )
 
-{-| 
-@docs Model, Msg, RouteParams, route, Data, ActionData
--}
-
-
-import BackendTask
+import BackendTask exposing (BackendTask)
+import Dict
 import Effect
-import ErrorPage
-import FatalError
+import FatalError exposing (FatalError)
 import Head
+import Head.Seo as Seo
 import Html
+import Html.Attributes
 import PagesMsg
-import Path
+import Route
+import Route.Articles
+import Route.Twilogs
 import RouteBuilder
-import Server.Request
-import Server.Response
-import Shared
+import Shared exposing (RataDie, Twilog, seoBase)
 import View
 
 
@@ -25,30 +29,62 @@ type alias Model =
 
 
 type Msg
-    = NoOp
+    = InitiateLinkPreviewPopulation
 
 
 type alias RouteParams =
     {}
 
 
+type alias Data =
+    { rataDie : RataDie
+    , twilogs : List Twilog
+    }
+
+
+type alias ActionData =
+    {}
+
+
 route : RouteBuilder.StatefulRoute RouteParams Data ActionData Model Msg
 route =
-    RouteBuilder.serverRender { data = data, action = action, head = head }
-        |> RouteBuilder.buildWithLocalState
-            { view = view
-            , init = init
+    RouteBuilder.single
+        { head = head
+        , data = data
+        }
+        |> RouteBuilder.buildWithSharedState
+            { init = \_ _ -> ( {}, Effect.init InitiateLinkPreviewPopulation )
             , update = update
-            , subscriptions = subscriptions
+            , subscriptions = \_ _ _ _ -> Sub.none
+            , view = view
             }
 
 
-init :
-    RouteBuilder.App Data ActionData RouteParams
-    -> Shared.Model
-    -> ( Model, Effect.Effect Msg )
-init app shared =
-    ( {}, Effect.none )
+data : BackendTask FatalError Data
+data =
+    Shared.twilogArchives
+        |> BackendTask.andThen
+            (\twilogArchives ->
+                case twilogArchives of
+                    latestArchive :: _ ->
+                        Shared.dailyTwilogsFromOldest [ latestArchive.path ]
+                            |> BackendTask.map
+                                (\dailyTwilogs ->
+                                    -- In this page dailyTwilogs contain only one day
+                                    Dict.get latestArchive.rataDie dailyTwilogs
+                                        |> Maybe.withDefault []
+                                        |> Data latestArchive.rataDie
+                                )
+
+                    [] ->
+                        BackendTask.fail (FatalError.fromString "No twilogs; Should not happen")
+            )
+
+
+head : RouteBuilder.App Data ActionData RouteParams -> List Head.Tag
+head _ =
+    Seo.summaryLarge seoBase
+        |> Seo.website
 
 
 update :
@@ -56,36 +92,14 @@ update :
     -> Shared.Model
     -> Msg
     -> Model
-    -> ( Model, Effect.Effect Msg )
+    -> ( Model, Effect.Effect Msg, Maybe Shared.Msg )
 update app shared msg model =
     case msg of
-        NoOp ->
-            ( model, Effect.none )
-
-
-subscriptions : RouteParams -> Path.Path -> Shared.Model -> Model -> Sub Msg
-subscriptions routeParams path shared model =
-    Sub.none
-
-
-type alias Data =
-    {}
-
-
-type alias ActionData =
-    {}
-
-
-data :
-    RouteParams
-    -> Server.Request.Parser (BackendTask.BackendTask FatalError.FatalError (Server.Response.Response Data ErrorPage.ErrorPage))
-data routeParams =
-    Server.Request.succeed (BackendTask.succeed (Server.Response.render {}))
-
-
-head : RouteBuilder.App Data ActionData RouteParams -> List Head.Tag
-head app =
-    []
+        InitiateLinkPreviewPopulation ->
+            ( model
+            , Effect.none
+            , Route.Twilogs.listUrlsForPreviewSingle shared app.data.twilogs
+            )
 
 
 view :
@@ -93,12 +107,87 @@ view :
     -> Shared.Model
     -> Model
     -> View.View (PagesMsg.PagesMsg Msg)
-view app shared model =
-    { title = "Index", body = [ Html.h2 [] [ Html.text "New Page" ] ] }
+view app shared _ =
+    { title = ""
+    , body =
+        [ Html.h1 []
+            [ View.imgLazy [ Html.Attributes.src <| Shared.ogpHeaderImageUrl ++ "?w=750&h=250", Html.Attributes.width 750, Html.Attributes.height 250, Html.Attributes.alt "Mt. Asama Header Image" ] []
+            , Html.text "ymtszw's page"
+            ]
+        , Html.h2 [] [ Route.link [] [ Html.text "Twilog" ] Route.Twilogs ]
+        , app.data.twilogs
+            |> Route.Twilogs.twilogsOfTheDay shared
+            |> showless "latest-twilogs"
+        , Html.h2 [] [ Route.link [] [ Html.text "記事" ] Route.Articles, View.feedLink "/articles/feed.xml" ]
+        , app.sharedData.cmsArticles
+            |> List.take 5
+            |> List.map Route.Articles.cmsArticlePreview
+            |> Html.div []
+            |> showless "cms-articles"
+        , Html.h2 [] [ Html.text "Zenn記事", View.feedLink "https://zenn.dev/ymtszw/feed" ]
+        , app.sharedData.zennArticles
+            |> List.sortBy (.likedCount >> negate)
+            |> List.map
+                (\metadata ->
+                    Html.li []
+                        [ Html.strong [] [ externalLink metadata.url metadata.title ]
+                        , Html.br [] []
+                        , Html.small []
+                            [ Html.strong [] [ Html.text (String.fromInt metadata.likedCount) ]
+                            , Html.text " 💚"
+                            , Html.code [] [ Html.text metadata.articleType ]
+                            , Html.text " ["
+                            , Html.text (Shared.posixToYmd metadata.publishedAt)
+                            , Html.text "]"
+                            ]
+                        ]
+                )
+            |> Html.ul []
+            |> showless "zenn-articles"
+        , Html.h2 [] [ Html.text "Qiita記事", View.feedLink "https://qiita.com/ymtszw/feed" ]
+        , app.sharedData.qiitaArticles
+            |> List.sortBy (.likesCount >> negate)
+            |> List.map
+                (\metadata ->
+                    Html.li []
+                        [ Html.strong [] [ externalLink metadata.url metadata.title ]
+                        , Html.br [] []
+                        , Html.small []
+                            [ Html.strong [] [ Html.text (String.fromInt metadata.likesCount) ]
+                            , Html.text " ✅"
+                            , Html.code [] (List.map Html.text (List.intersperse ", " metadata.tags))
+                            , Html.text " ["
+                            , Html.text (Shared.posixToYmd metadata.createdAt)
+                            , Html.text "]"
+                            ]
+                        ]
+                )
+            |> Html.ul []
+            |> showless "qiita-articles"
+        , Html.h2 [] [ Html.text "GitHub Public Repo" ]
+        , app.sharedData.repos
+            |> List.map
+                (\publicOriginalRepo ->
+                    Html.strong []
+                        [ Html.text "["
+                        , externalLink ("https://github.com/ymtszw/" ++ publicOriginalRepo) publicOriginalRepo
+                        , Html.text "]"
+                        ]
+                )
+            |> List.intersperse (Html.text " ")
+            |> Html.p []
+            |> showless "repos"
+        ]
+    }
 
 
-action :
-    RouteParams
-    -> Server.Request.Parser (BackendTask.BackendTask FatalError.FatalError (Server.Response.Response ActionData ErrorPage.ErrorPage))
-action routeParams =
-    Server.Request.succeed (BackendTask.succeed (Server.Response.render {}))
+showless id inner =
+    Html.div []
+        [ Html.input [ Html.Attributes.id id, Html.Attributes.type_ "checkbox", Html.Attributes.class "showless-toggle" ] []
+        , inner
+        , Html.label [ Html.Attributes.for id, Html.Attributes.class "showless-button" ] []
+        ]
+
+
+externalLink url text_ =
+    Html.a [ Html.Attributes.href url, Html.Attributes.target "_blank" ] [ Html.text text_ ]
