ffc16cce2547a3f93b35b1259d90c63819f9b52b
Yu Matsuzawa <ymtszw@gmail.com>
2023-04-08 23:52:50 +0900

    feat: Twilogs.Day_ページを移行

--- app/Route/Twilogs/Day_.elm
+++ app/Route/Twilogs/Day_.elm
@@ -1,22 +1,27 @@
-module Route.Twilogs.Day_ exposing (ActionData, Data, Model, Msg, RouteParams, route)
+module Route.Twilogs.Day_ exposing
+    ( ActionData
+    , Data
+    , Model
+    , Msg
+    , RouteParams
+    , route
+    )
 
-{-| 
-@docs Model, Msg, RouteParams, route, Data, ActionData
--}
-
-
-import BackendTask
+import BackendTask exposing (BackendTask)
+import Date
+import Dict
 import Effect
-import ErrorPage
-import FatalError
+import FatalError exposing (FatalError)
 import Head
-import Html
+import Head.Seo as Seo
+import Html exposing (Html, nav, strong, text)
+import Html.Attributes exposing (class)
+import List.Extra
 import PagesMsg
-import Path
+import Route
+import Route.Twilogs
 import RouteBuilder
-import Server.Request
-import Server.Response
-import Shared
+import Shared exposing (RataDie, Twilog, seoBase)
 import View
 
 
@@ -25,30 +30,77 @@ type alias Model =
 
 
 type Msg
-    = NoOp
+    = InitiateLinkPreviewPopulation
 
 
 type alias RouteParams =
     { day : String }
 
 
+type alias Data =
+    { rataDie : RataDie
+    , twilogs : List Twilog
+    }
+
+
+type alias ActionData =
+    {}
+
+
 route : RouteBuilder.StatefulRoute RouteParams Data ActionData Model Msg
 route =
-    RouteBuilder.serverRender { data = data, action = action, head = head }
-        |> RouteBuilder.buildWithLocalState
-            { view = view
-            , init = init
+    RouteBuilder.preRender
+        { head = head
+        , pages = pages
+        , data = data
+        }
+        |> RouteBuilder.buildWithSharedState
+            { init = \_ _ -> ( {}, Effect.init InitiateLinkPreviewPopulation )
             , update = update
-            , subscriptions = subscriptions
+            , subscriptions = \_ _ _ _ -> Sub.none
+            , view = view
             }
 
 
-init :
+pages : BackendTask FatalError (List RouteParams)
+pages =
+    BackendTask.map (List.map (.isoDate >> RouteParams)) Shared.twilogArchives
+
+
+data : RouteParams -> BackendTask FatalError Data
+data routeParams =
+    Date.fromIsoString routeParams.day
+        |> BackendTask.fromResult
+        |> BackendTask.mapError FatalError.fromString
+        |> BackendTask.andThen
+            (\date ->
+                Shared.dailyTwilogsFromOldest [ Shared.makeTwilogsJsonPath date ]
+                    |> BackendTask.map
+                        (\dailyTwilogs ->
+                            -- In this page dailyTwilogs contain only one day
+                            Dict.get (Date.toRataDie date) dailyTwilogs
+                                |> Maybe.withDefault []
+                                |> Data (Date.toRataDie date)
+                        )
+            )
+
+
+head :
     RouteBuilder.App Data ActionData RouteParams
-    -> Shared.Model
-    -> ( Model, Effect.Effect Msg )
-init app shared =
-    ( {}, Effect.none )
+    -> List Head.Tag
+head app =
+    Seo.summaryLarge
+        { seoBase
+            | title = Shared.makeTitle (app.routeParams.day ++ "のTwilog")
+            , description = app.routeParams.day ++ "のTwilog"
+        }
+        |> Seo.article
+            { tags = []
+            , section = Nothing
+            , publishedTime = Nothing
+            , modifiedTime = Nothing
+            , expirationTime = Nothing
+            }
 
 
 update :
@@ -56,36 +108,14 @@ update :
     -> Shared.Model
     -> Msg
     -> Model
-    -> ( Model, Effect.Effect Msg )
+    -> ( Model, Effect.Effect Msg, Maybe Shared.Msg )
 update app shared msg model =
     case msg of
-        NoOp ->
-            ( model, Effect.none )
-
-
-subscriptions : RouteParams -> Path.Path -> Shared.Model -> Model -> Sub Msg
-subscriptions routeParams path shared model =
-    Sub.none
-
-
-type alias Data =
-    {}
-
-
-type alias ActionData =
-    {}
-
-
-data :
-    RouteParams
-    -> Server.Request.Parser (BackendTask.BackendTask FatalError.FatalError (Server.Response.Response Data ErrorPage.ErrorPage))
-data routeParams =
-    Server.Request.succeed (BackendTask.succeed (Server.Response.render {}))
-
-
-head : RouteBuilder.App Data ActionData RouteParams -> List Head.Tag
-head app =
-    []
+        InitiateLinkPreviewPopulation ->
+            ( model
+            , Effect.none
+            , Route.Twilogs.listUrlsForPreviewSingle shared app.data.twilogs
+            )
 
 
 view :
@@ -93,12 +123,74 @@ view :
     -> Shared.Model
     -> Model
     -> View.View (PagesMsg.PagesMsg Msg)
-view app shared model =
-    { title = "Twilogs.Day_", body = [ Html.h2 [] [ Html.text "New Page" ] ] }
+view app shared _ =
+    { title = app.routeParams.day ++ "のTwilog"
+    , body =
+        [ -- show navigation links to previous and next days
+          prevNextNavigation app.data app.sharedData.twilogArchives
+        , Route.Twilogs.twilogDailySection shared app.data.rataDie app.data.twilogs
+        , prevNextNavigation app.data app.sharedData.twilogArchives
+        , Route.Twilogs.linksByMonths (Just (Date.fromRataDie app.data.rataDie)) app.sharedData.twilogArchives
+        ]
+    }
 
 
-action :
-    RouteParams
-    -> Server.Request.Parser (BackendTask.BackendTask FatalError.FatalError (Server.Response.Response ActionData ErrorPage.ErrorPage))
-action routeParams =
-    Server.Request.succeed (BackendTask.succeed (Server.Response.render {}))
+prevNextNavigation : Data -> List Shared.TwilogArchiveMetadata -> Html msg
+prevNextNavigation { rataDie } twilogArchives =
+    let
+        toLink maybeRataDie child =
+            case maybeRataDie of
+                Just rataDie_ ->
+                    Route.link [] [ strong [] [ child ] ] <| Route.Twilogs__Day_ { day = Date.toIsoString (Date.fromRataDie rataDie_) }
+
+                Nothing ->
+                    child
+    in
+    nav [ class "prev-next-navigation" ]
+        [ toLink (findPrevRataDie rataDie twilogArchives) <| text "← 前"
+        , toLink (findNextRataDie rataDie twilogArchives) <| text "次 →"
+        ]
+
+
+findNextRataDie : RataDie -> List Shared.TwilogArchiveMetadata -> Maybe RataDie
+findNextRataDie today twilogArchives =
+    case List.reverse twilogArchives of
+        (oldestArchive :: _) as reversedArchives ->
+            if today < oldestArchive.rataDie then
+                -- Enter into range from oldest
+                Just oldestArchive.rataDie
+
+            else
+                case List.Extra.dropWhile (\a -> a.rataDie <= today) reversedArchives of
+                    nearestNextArchive :: _ ->
+                        Just nearestNextArchive.rataDie
+
+                    [] ->
+                        -- Today is the latest
+                        Nothing
+
+        [] ->
+            -- Usually won't happen
+            Nothing
+
+
+findPrevRataDie : RataDie -> List Shared.TwilogArchiveMetadata -> Maybe RataDie
+findPrevRataDie today twilogArchives =
+    case twilogArchives of
+        latestArchive :: _ ->
+            if today > latestArchive.rataDie then
+                -- Enter into range from latest
+                Just latestArchive.rataDie
+
+            else
+                case List.Extra.dropWhile (\a -> a.rataDie >= today) twilogArchives of
+                    nearestPrevArchive :: _ ->
+                        Just nearestPrevArchive.rataDie
+
+                    [] ->
+                        -- Today is the oldest
+                        Nothing
+
+        [] ->
+            -- Usually won't happen
+            Nothing
