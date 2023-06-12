3ec9cc7ff0521f44f7903444376285d96ec5bfea
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2023-06-12 19:51:48 +0900

    feat: impl makeAmazonUrl with QS

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,10 +1,12 @@
-module Helper exposing (initMsg, iso8601Decoder, nonEmptyString, waitMsg)
+module Helper exposing (initMsg, iso8601Decoder, makeAmazonUrl, nonEmptyString, waitMsg)
 
 import Iso8601
 import OptimizedDecoder
 import Process
+import QS
 import Task
 import Time
+import Url
 
 
 iso8601Decoder : OptimizedDecoder.Decoder Time.Posix
@@ -33,3 +35,34 @@ initMsg =
 waitMsg : Float -> msg -> Cmd msg
 waitMsg ms msg =
     Task.perform (\() -> msg) (Process.sleep ms)
+
+
+makeAmazonUrl : String -> String -> String
+makeAmazonUrl amazonAssociateTag rawUrl =
+    Url.fromString rawUrl
+        |> Maybe.map
+            (\url ->
+                case ( url.host, String.contains "/dp/" url.path ) of
+                    ( "www.amazon.co.jp", True ) ->
+                        embedTag amazonAssociateTag url
+
+                    _ ->
+                        rawUrl
+            )
+        |> Maybe.withDefault rawUrl
+
+
+embedTag : String -> Url.Url -> String
+embedTag amazonAssociateTag url =
+    url.query
+        |> Maybe.withDefault ""
+        |> QS.parse QS.config
+        |> (\qs ->
+                { url
+                    | query =
+                        QS.setStr "tag" amazonAssociateTag qs
+                            |> QS.serialize QS.config
+                            |> Just
+                }
+           )
+        |> Url.toString
