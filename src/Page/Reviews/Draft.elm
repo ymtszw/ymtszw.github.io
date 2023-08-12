d1f4ad22c5f5a2b43d0cbcb6753df6eb0280c163
Yu Matsuzawa <ymtszw@gmail.com>
2023-08-13 04:50:07 +0900

    feat: show current drafting review

--- src/Page/Reviews/Draft.elm
+++ src/Page/Reviews/Draft.elm
@@ -6,7 +6,8 @@ import DataSource.Env
 import Dict exposing (Dict)
 import Head
 import Helper
-import Html exposing (Html)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 import KindleBook exposing (ASIN, KindleBook, kindleBooks)
 import LinkPreview
 import Markdown
@@ -45,12 +46,16 @@ type alias RouteParams =
 type alias Data =
     { kindleBooks : Dict ASIN KindleBook
     , amazonAssociateTag : String
+    , secrets : KindleBook.Secrets
     }
 
 
 data : DataSource Data
 data =
-    DataSource.map2 Data kindleBooks (DataSource.Env.load "AMAZON_ASSOCIATE_TAG")
+    DataSource.map3 Data
+        kindleBooks
+        (DataSource.Env.load "AMAZON_ASSOCIATE_TAG")
+        KindleBook.secrets
 
 
 head : StaticPayload Data RouteParams -> List Head.Tag
@@ -75,12 +80,22 @@ type alias Model =
 
 init : Maybe PageUrl -> Shared.Model -> StaticPayload Data RouteParams -> ( Model, Cmd Msg )
 init pageUrl _ static =
-    ( getStaticBook pageUrl static.data.kindleBooks
-    , Helper.initMsg StartLinkPreview
-    )
+    case getStaticBook pageUrl static.data.kindleBooks of
+        Just book ->
+            ( bookToModel book
+            , KindleBook.getOnDemand Res_refreshKindleBookOnDemand static.data.secrets book.id
+            )
+
+        Nothing ->
+            ( Model Nothing "タイトル" sample Nothing Nothing, Cmd.none )
 
 
-getStaticBook : Maybe PageUrl -> Dict ASIN KindleBook -> Model
+bookToModel : KindleBook -> Model
+bookToModel book =
+    Model (Just book) (Maybe.withDefault "タイトル" book.reviewTitle) (Maybe.withDefault sample book.reviewMarkdown) book.reviewUpdatedAt book.reviewPublishedAt
+
+
+getStaticBook : Maybe PageUrl -> Dict ASIN KindleBook -> Maybe KindleBook
 getStaticBook pageUrl books =
     pageUrl
         |> Maybe.andThen .query
@@ -88,12 +103,26 @@ getStaticBook pageUrl books =
         |> Maybe.andThen (Dict.get "id")
         |> Maybe.andThen List.head
         |> Maybe.andThen (\id -> Dict.get id books)
-        |> Maybe.map (\book -> Model (Just book) (Maybe.withDefault "タイトル" book.reviewTitle) (Maybe.withDefault "本文" book.reviewMarkdown) book.reviewUpdatedAt book.reviewPublishedAt)
-        |> Maybe.withDefault (Model Nothing "タイトル" "本文" Nothing Nothing)
+
+
+sample =
+    """
+## アカリがやってきたぞ！
+
+うおー
+
+https://twitter.com/gada_twt/status/1690322717642457088
+
+どうにかなーれ
+
+https://snowbreak.amazingseasun.com/#/?id=1
+
+
+"""
 
 
 type Msg
-    = StartLinkPreview
+    = Res_refreshKindleBookOnDemand (Result String KindleBook)
 
 
 update :
@@ -106,15 +135,21 @@ update :
     -> ( Model, Cmd Msg, Maybe Shared.Msg )
 update _ _ _ static msg m =
     case msg of
-        StartLinkPreview ->
-            ( m
-            , Cmd.none
-            , m.reviewMarkdown
-                |> Markdown.parseAndEnumerateLinks
-                |> Shared.Req_LinkPreview static.data.amazonAssociateTag
-                |> Shared.SharedMsg
-                |> Just
-            )
+        Res_refreshKindleBookOnDemand (Ok book) ->
+            bookToModel book
+                |> (\updated ->
+                        ( updated
+                        , Cmd.none
+                        , updated.reviewMarkdown
+                            |> Markdown.parseAndEnumerateLinks
+                            |> Shared.Req_LinkPreview static.data.amazonAssociateTag
+                            |> Shared.SharedMsg
+                            |> Just
+                        )
+                   )
+
+        Res_refreshKindleBookOnDemand (Err _) ->
+            ( m, Cmd.none, Nothing )
 
 
 view :
@@ -123,42 +158,83 @@ view :
     -> Model
     -> StaticPayload Data RouteParams
     -> View Msg
-view _ { links } m _ =
+view _ { links } m static =
     { title = "レビュー（下書き）"
     , body =
-        let
-            timestamp =
-                Html.small [] <|
-                    case m.reviewPublishedAt of
-                        Just pa ->
-                            Html.text ("公開: " ++ Shared.formatPosix pa)
-                                :: (case m.reviewUpdatedAt of
-                                        Just ua ->
-                                            [ Html.text (" (更新: " ++ Shared.formatPosix ua ++ ")") ]
+        [ case m.book of
+            Just book ->
+                renderReview static.data.amazonAssociateTag links book m
 
-                                        Nothing ->
-                                            []
-                                   )
-
-                        Nothing ->
-                            [ Html.text "未公開" ]
-        in
-        [ Html.header [] [ timestamp ]
-        , renderReview links m
-        , Html.div [] [ timestamp ]
+            Nothing ->
+                Html.div [] []
         ]
     }
 
 
 renderReview :
-    Dict String LinkPreview.Metadata
-    ->
-        { a
-            | reviewTitle : String
-            , reviewMarkdown : String
-        }
+    String
+    -> Dict String LinkPreview.Metadata
+    -> KindleBook
+    -> Model
     -> Html msg
-renderReview links m =
+renderReview amazonAssociateTag links book m =
+    let
+        timestamp =
+            Html.small [] <|
+                case m.reviewPublishedAt of
+                    Just pa ->
+                        Html.text ("公開: " ++ Shared.formatPosix pa)
+                            :: (case m.reviewUpdatedAt of
+                                    Just ua ->
+                                        [ Html.text (" (更新: " ++ Shared.formatPosix ua ++ ")") ]
+
+                                    Nothing ->
+                                        []
+                               )
+
+                    Nothing ->
+                        [ Html.text "未公開" ]
+    in
     Html.article [] <|
-        Html.h1 [] [ Html.text m.reviewTitle ]
-            :: Markdown.parseAndRender links m.reviewMarkdown
+        let
+            optionalVolume =
+                if book.volume <= 0 then
+                    ""
+
+                else
+                    "（" ++ String.fromInt book.volume ++ "）"
+        in
+        [ Html.h1 [] [ Html.text <| "レビュー：" ++ book.seriesName ++ optionalVolume ]
+        , Html.div [] [ timestamp ]
+        , bookDetails amazonAssociateTag book
+        , Html.h1 [] [ Html.text m.reviewTitle ]
+        ]
+            ++ Markdown.parseAndRender links m.reviewMarkdown
+            ++ [ Html.div [] [ timestamp ]
+               , bookDetails amazonAssociateTag book
+               ]
+
+
+bookDetails amazonAssociateTag book =
+    blockquote [ class "kindle-book-details" ]
+        [ a [ href (Helper.makeAmazonUrl amazonAssociateTag book.id), target "_blank", class "has-image" ]
+            [ View.imgLazy [ src book.img, width 150, alt <| book.rawTitle ++ "の書影" ] [] ]
+        , div []
+            [ h5 [] [ a [ href (Helper.makeAmazonUrl amazonAssociateTag book.id), target "_blank" ] [ text book.rawTitle ] ]
+            , ul [] <|
+                List.filterMap (Maybe.map (\( key, kids ) -> li [] (strong [] [ text key ] :: text " : " :: kids)))
+                    [ Just ( "著者", List.map tag book.authors )
+                    , if book.seriesName == book.rawTitle then
+                        Nothing
+
+                      else
+                        Just ( "シリーズ", [ text book.seriesName ] )
+                    , Maybe.map (\label_ -> ( "レーベル", [ tag label_ ] )) book.label
+                    , Just ( "購入日", [ text (Helper.toJapaneseDate book.acquiredDate) ] )
+                    ]
+            ]
+        ]
+
+
+tag word =
+    span [ class "kindle-filterable-tag" ] [ text word ]
