8c58f6e269ca0204afff0e2a372c74264904c733
Yu Matsuzawa <yu.matsuzawa@siiibo.com>
2023-12-05 19:54:52 +0900

    fix: typo

--- src/Page/Library.elm
+++ src/Page/Library.elm
@@ -529,7 +529,7 @@ kindleSearchBox { searchResults, searching } =
 
             _ ->
                 div [ class "search-results" ]
-                    [ small [] [ text ("約" ++ String.fromInt searchResults.estimatedTotalHits ++ "件ヒット（最大100件で表示）") ]
+                    [ small [] [ text ("約" ++ String.fromInt searchResults.estimatedTotalHits ++ "件ヒット（最大100件表示）") ]
                     , div
                         [ class "provider" ]
                         [ text "Powered by "
