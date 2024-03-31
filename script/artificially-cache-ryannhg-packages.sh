40e0e2c52ee8d4eaa4e9bfc4837e6bf0806d4064
Yu Matsuzawa <ymtszw@gmail.com>
2024-03-31 15:26:57 +0900

    fix: ensure cache directory

--- script/artificially-cache-ryannhg-packages.sh
+++ script/artificially-cache-ryannhg-packages.sh
@@ -9,6 +9,7 @@ if [ -d "$target_directory/ryannhg" ]; then
   exit 0
 else
   echo " ryannhg packages are not cached. Injecting..."
+  mkdir -p $target_directory
   tar -xzvf "$(dirname $0)/ryannhg.tar.gz" -C "$target_directory/"
   echo " ryannhg packages are now cached."
 fi
