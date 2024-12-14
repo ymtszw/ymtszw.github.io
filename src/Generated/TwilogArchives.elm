b539cdda317ae9ee4efbd2a348c5aab9e55a66ed
Yu Matsuzawa <ymtszw@gmail.com>
2024-12-15 04:51:55 +0900

    feat: imported recent twilogs (Sun Dec 15 04:51:55 JST 2024)

--- src/Generated/TwilogArchives.elm
+++ src/Generated/TwilogArchives.elm
@@ -7,7 +7,8 @@ type alias TwilogArchiveYearMonth =
 
 list : List TwilogArchiveYearMonth
 list =
-    [ "2024-11"
+    [ "2024-12"
+    , "2024-11"
     , "2024-10"
     , "2024-09"
     , "2024-08"
