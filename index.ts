276d9a66753ca2e1ceb51ff6cbf3e46bf76e310c
Yu Matsuzawa <ymtszw@gmail.com>
2025-02-03 02:40:35 +0900

    chore: move implementation of public/index.js to index.ts

--- index.ts
+++ index.ts
@@ -3,13 +3,66 @@ type ElmPagesInit = {
   flags: unknown;
 };
 
+type App = {
+  ports: {
+    toJs: {
+      subscribe: (
+        callback: (data: { tag: string; value: string }) => unknown
+      ) => unknown;
+    };
+    fromJs: {
+      send: (data: {
+        viewportHeight: number;
+        viewportTop: number;
+        viewportBottom: number;
+      }) => unknown;
+    };
+  };
+};
+
 const config: ElmPagesInit = {
   load: async function (elmLoaded) {
-    const app = await elmLoaded;
+    const app = (await elmLoaded) as App;
     console.log("App loaded", app);
+    app.ports.toJs.subscribe((data: { tag: string; value: string }) => {
+      switch (data.tag) {
+        case "StoreLibraryKey":
+          localStorage.setItem("LibraryKey", data.value);
+          console.log("LibraryKey stored");
+          break;
+        default:
+          console.log("Unknown message type", data);
+      }
+    });
+
+    let ticking = 0;
+    window.addEventListener(
+      "scroll",
+      (event) => {
+        if (ticking > 5) {
+          window.requestAnimationFrame(() => {
+            app.ports.fromJs.send({
+              viewportHeight: window.innerHeight,
+              viewportTop: window.scrollY,
+              viewportBottom: window.scrollY + window.innerHeight,
+            });
+            ticking = 0;
+          });
+        }
+        ticking += 1;
+      },
+      { passive: true }
+    );
   },
   flags: function () {
-    return "You can decode this in Shared.elm using Json.Decode.string!";
+    return {
+      libraryKey: localStorage.getItem("LibraryKey"),
+      viewport: {
+        viewportHeight: window.innerHeight,
+        viewportTop: window.scrollY,
+        viewportBottom: window.scrollY + window.innerHeight,
+      },
+    };
   },
 };
 
