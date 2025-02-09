31c453466b36f9fd44166a98e543c6d12a929cd3
Yu Matsuzawa <ymtszw@gmail.com>
2025-02-10 06:22:22 +0900

    feat: migrate getImageDimensions

--- custom-backend-task.ts
+++ custom-backend-task.ts
@@ -1,3 +1,21 @@
-export async function hello(name) {
-  return `Hello ${name}!`;
+import imageSizeFromFile from "image-size";
+
+/**
+ * Implementation of `BackendTask.File.Extra.getImageDimensions`
+ *
+ * @param { string } fileName
+ * @returns { Promise<{ width: number, height: number }> }
+ */
+export async function getImageDimensions(
+  fileName: string
+): Promise<{ width: number; height: number }> {
+  const dimensions = imageSizeFromFile(`public/${fileName.replace(/^\//, "")}`);
+  if (dimensions.width && dimensions.height) {
+    return {
+      width: dimensions.width,
+      height: dimensions.height,
+    };
+  } else {
+    throw new Error("Invalid image dimensions");
+  }
 }
